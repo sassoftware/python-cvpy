d58be1d7084763c1201e87170a5a117b36877829
ambrozicc1 <44813043+ambrozicc1@users.noreply.github.com>
2021-04-09 16:59:44 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ def get_file(fname):
 
 setup(
     name='cvpy',
-    version='1.0.0',
+    version='0.1.0',
     description='SAS Computer Vision Interface',
     long_description=get_file('README.md'),
     long_description_content_type='text/markdown',
